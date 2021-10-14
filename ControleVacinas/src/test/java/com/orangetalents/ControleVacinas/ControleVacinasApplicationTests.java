3e6335b654e2ba1747b252897ef9c55258f52c19
LaisLimaSantos <laissp25@gmail.com>
2021-10-13 23:47:46 -0300

    backup

---
+++ ControleVacinas/src/test/java/com/orangetalents/ControleVacinas/ControleVacinasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orangetalents.ControleVacinas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleVacinasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
